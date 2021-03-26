612e73032585d49c8419edb8fd4ca5cc6bc332c3
Joshua J. Bouw <dev@joshuajbouw.com>
2021-03-26 17:29:48 +0700

    add env fix

--- runtime/runtime-params-estimator/src/lib.rs
+++ runtime/runtime-params-estimator/src/lib.rs
@@ -38,8 +38,8 @@ impl std::ops::Deref for TestContract {
     fn deref(&self) -> &[u8] {
         self.cell
             .get_or_init(|| {
-                let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-                let path = Path::new(dir.as_str()).join(self.path);
+                let dir = env!("CARGO_MANIFEST_DIR");
+                let path = Path::new(dir).join(self.path);
                 std::fs::read(&path).unwrap_or_else(|_err| {
                     panic!("failed to load test resource: {}", path.display())
                 })
